47bd6bc8415d131e26d0583fbeae97cac5804525
Vinícius Cardoso <vinicius.mct17@gmail.com>
2022-06-04 21:53:12 -0300

    Implementando a logica principal do jogo

--- tp1_v2/include/Jogador.hpp
+++ tp1_v2/include/Jogador.hpp
@@ -15,18 +15,24 @@ class Jogador {
     string nome;
     int dinheiro;
     int valor_aposta;
+    int valor_mao;
     Carta mao[NUMERO_DE_CARTAS];
 
    public:
     Jogador();
     Jogador(string nome, int dinheiro, int valor_aposta);
 
+    void SetNome(string nome);
     string GetNome();
+    void SetDinheiro(int dinheiro);
     int GetDinheiro();
-    void SetAposta(int aposta);
+    bool SetAposta(int aposta);
+    void SetValorMao(string valor_mao);
+    int GetValorMao();
+    string *GetMao();
     void AdicionarCarta(string _carta);
     void ImprimirMao();
-    string GetMao();
+    void LimparMao();
 };
 
 Jogador::Jogador() {
@@ -41,16 +47,57 @@ Jogador::Jogador(string nome, int dinheiro, int valor_aposta) {
     this->valor_aposta = valor_aposta;
 }
 
+void Jogador::SetNome(string nome) {
+    this->nome = nome;
+}
+
 string Jogador::GetNome() {
     return this->nome;
+}
+
+void Jogador::SetDinheiro(int dinheiro) {
+    this->dinheiro = dinheiro;
 }
 
 int Jogador::GetDinheiro() {
     return this->dinheiro;
 }
 
-void Jogador::SetAposta(int aposta) {
+bool Jogador::SetAposta(int aposta) {
     this->valor_aposta = aposta;
+    this->dinheiro -= this->valor_aposta;
+
+    bool condicao;
+    condicao = this->dinheiro < 0 ? false : true;
+
+    return condicao;
+}
+
+void Jogador::SetValorMao(string valor_mao) {
+    if (valor_mao == "HC")
+        this->valor_mao = 1;
+    else if (valor_mao == "OP")
+        this->valor_mao = 2;
+    else if (valor_mao == "TP")
+        this->valor_mao = 3;
+    else if (valor_mao == "TK")
+        this->valor_mao = 4;
+    else if (valor_mao == "S")
+        this->valor_mao = 5;
+    else if (valor_mao == "F")
+        this->valor_mao = 6;
+    else if (valor_mao == "FH")
+        this->valor_mao = 7;
+    else if (valor_mao == "FK")
+        this->valor_mao = 8;
+    else if (valor_mao == "SF")
+        this->valor_mao = 9;
+    else if (valor_mao == "RSF")
+        this->valor_mao = 10;
+}
+
+int Jogador::GetValorMao() {
+    return this->valor_mao;
 }
 
 void Jogador::AdicionarCarta(string _carta) {
@@ -84,9 +131,12 @@ void Jogador::ImprimirMao() {
     cout << endl;
 }
 
-string Jogador::GetMao() {
+string *Jogador::GetMao() {
     int repeticoes_valor = 0, repeticoes_naipe = 0;
+    string *detalhes_mao = new string[3];
     string mao_avaliada = "";
+    string maior_carta_avaliada = "";
+    bool trinca = false;
 
     // o for eh invertido porque deseja-se obter a mao com as cartas mais valiosas
     for (int i = NUMERO_DE_CARTAS; i > 0; i--) {
@@ -94,8 +144,10 @@ string Jogador::GetMao() {
             repeticoes_valor++;
 
             if (repeticoes_valor == 2) {
-                if (mao[i - 1].GetValor() == mao[i + 1].GetValor())
+                if (mao[i - 1].GetValor() == mao[i + 1].GetValor()) {
                     repeticoes_valor++;
+                    trinca = true;
+                }
             }
         }
 
@@ -104,24 +156,12 @@ string Jogador::GetMao() {
         }
     }
 
-    // verificar se eh um Straight e se pode ser um Straight Flush
-    if (repeticoes_valor == 0) {
-        if ((mao[NUMERO_DE_CARTAS - 1].GetValor() - mao[0].GetValor()) == 4)
-            mao_avaliada = "S";
-        if ((mao[NUMERO_DE_CARTAS - 1].GetValor() - mao[1].GetValor()) == 3) {
-            if (mao[NUMERO_DE_CARTAS - 1].GetValor() == 13 && mao[0].GetValor() == 1)
-                mao_avaliada = "RS";  // Royal Straight eh auxiliar e temporario
+    if (repeticoes_valor == 1 || ((repeticoes_valor == 2) && trinca == false)) {
+        for (int i = 0; i < NUMERO_DE_CARTAS; i++) {
+            if (mao[i].GetValor() == mao[i + 1].GetValor()) {
+                maior_carta_avaliada = mao[i + 1].GetCarta();
+            }
         }
-    }
-
-    // verificar se eh um Flush ou Straight Flush
-    if (repeticoes_naipe == 4) {
-        if (mao_avaliada == "S")
-            mao_avaliada = "SF";  // Straight Flush
-        else if (mao_avaliada == "RS")
-            mao_avaliada = "RSF";  // Royal Straight Flush
-        else
-            mao_avaliada = "F";  // Flush
     }
 
     switch (repeticoes_valor) {
@@ -133,23 +173,75 @@ string Jogador::GetMao() {
             break;
         case 3:
             mao_avaliada = "TK";  // Three of a Kind
+            maior_carta_avaliada = mao[2].GetCarta();
             break;
         case 4:
             mao_avaliada = "FK";  // Four of a Kind
+            maior_carta_avaliada = mao[1].GetCarta();
             break;
+    }
+
+    // verificar se eh um Straight e se pode ser um Straight Flush
+    if (repeticoes_valor == 0) {
+        if ((mao[NUMERO_DE_CARTAS - 1].GetValor() - mao[0].GetValor()) == 4) {
+            mao_avaliada = "S";
+            maior_carta_avaliada = mao[NUMERO_DE_CARTAS - 1].GetCarta();
+        }
+        if ((mao[NUMERO_DE_CARTAS - 1].GetValor() - mao[1].GetValor()) == 3) {
+            if (mao[NUMERO_DE_CARTAS - 1].GetValor() == 13 && mao[0].GetValor() == 1)
+                mao_avaliada = "RS";  // Royal Straight eh auxiliar e temporario
+        }
+    }
+
+    // verificar se eh um Flush ou Straight Flush
+    if (repeticoes_naipe == 4) {
+        if (mao_avaliada == "S") {
+            mao_avaliada = "SF";  // Straight Flush
+
+            if (mao[NUMERO_DE_CARTAS - 1].GetValor() == 13 && mao[0].GetValor() == 1)
+                maior_carta_avaliada = mao[0].GetCarta();  // As
+            else
+                maior_carta_avaliada = mao[NUMERO_DE_CARTAS - 1].GetCarta();
+
+        } else if (mao_avaliada == "RS") {
+            mao_avaliada = "RSF";                      // Royal Straight Flush
+            maior_carta_avaliada = mao[0].GetCarta();  // As
+        } else {
+            mao_avaliada = "F";  // Flush
+
+            if (mao[NUMERO_DE_CARTAS - 1].GetValor() == 13 && mao[0].GetValor() == 1)
+                maior_carta_avaliada = mao[0].GetCarta();  // As
+            else
+                maior_carta_avaliada = mao[NUMERO_DE_CARTAS - 1].GetCarta();
+        }
     }
 
     // em uma quadra eh impossivel o segundo e o ultimo elemento serem diferentes
     // logo, se ha 4 repeticoes de valor, ha um full house
-    if (repeticoes_valor == 4 && (mao[1].GetValor() != mao[3].GetValor()))
+    if (repeticoes_valor == 4 && (mao[1].GetValor() != mao[3].GetValor())) {
         mao_avaliada = "FH";  // Full House
+        maior_carta_avaliada = mao[2].GetCarta();
+    }
+
+    string maior_carta_mao = mao[NUMERO_DE_CARTAS - 1].GetCarta();
 
     if (mao_avaliada == "RS")
         mao_avaliada = "S";
-    if (mao_avaliada == "")
+    if (mao_avaliada == "") {
         mao_avaliada = "HC";
+        maior_carta_avaliada = maior_carta_mao;
+    }
 
-    return mao_avaliada;
+    detalhes_mao[0] = mao_avaliada;
+    detalhes_mao[1] = maior_carta_avaliada;
+    detalhes_mao[2] = maior_carta_mao;
+
+    return detalhes_mao;
+}
+
+void Jogador::LimparMao() {
+    for (int i = 0; i < NUMERO_DE_CARTAS; i++)
+        mao[i].SetCarta("0Z");
 }
 
 #endif
--- tp1_v2/src/Main.cpp
+++ tp1_v2/src/Main.cpp
@@ -1,27 +1,118 @@
 #include <fstream>
 #include <iostream>
+#include <typeinfo>
 
 #include "Jogador.hpp"
 
 using namespace std;
 
+Jogador *GetVencedores(Jogador *jogadores, int numero_vencedores) {
+    int nr_vencedores = numero_vencedores;
+    int maior = 1, indice = nr_vencedores - 1;
+    int indice_aux = 0;
+
+    Jogador *empatados = new Jogador[nr_vencedores];
+
+    for (int i = 0; i < nr_vencedores; i++) {
+        // armazena em indice o indice do jogador com a maior mao
+        if (jogadores[i].GetValorMao() > maior) {
+            maior = jogadores[i].GetValorMao();
+            indice = i;
+            // se ha 2 jogadores com valor da mao igual, realiza o desempate
+        } else if (jogadores[i].GetValorMao() == maior) {
+            // desempate a partir da maior carta
+            if (jogadores[i].GetMao()[1] > jogadores[indice].GetMao()[1]) {
+                indice = i;
+                // impossivel desempatar
+            } else {
+                nr_vencedores++;
+                empatados[indice_aux] = jogadores[i];
+                indice_aux++;
+            }
+        }
+    }
+
+    return empatados;
+}
+
+Jogador GetVencedor(Jogador *jogadores, int numero_jogadores) {
+    int nr_jogadores = numero_jogadores;
+    int maior = 1, indice = nr_jogadores - 1;
+    int indice_aux = 0, nr_vencedores = 1;
+
+    for (int i = 0; i < nr_jogadores; i++) {
+        // armazena em indice o indice do jogador com a maior mao
+        if (jogadores[i].GetValorMao() > maior) {
+            maior = jogadores[i].GetValorMao();
+            indice = i;
+            // se ha 2 jogadores com valor da mao igual, realiza o desempate
+        } else if (jogadores[i].GetValorMao() == maior) {
+            // desempate a partir da maior carta
+            if (jogadores[i].GetMao()[1] > jogadores[indice].GetMao()[1])
+                indice = i;
+        }
+    }
+
+    return jogadores[indice];
+}
+
+int GetNrVencedores(Jogador *jogadores, int numero_jogadores) {
+    int nr_jogadores = numero_jogadores;
+    int maior = 0, indice = nr_jogadores - 1;
+    int nr_vencedores = 1;
+
+    for (int i = 0; i < nr_jogadores; i++) {
+        // armazena o indice do jogador com a maior mao
+        if (jogadores[i].GetValorMao() > maior) {
+            maior = jogadores[i].GetValorMao();
+            indice = i;
+            // se ha 2 jogadores com valor da mao igual, realiza o desempate
+        } else if (jogadores[i].GetValorMao() == maior) {
+            cout << jogadores[i].GetNome() << endl;
+            // desempate a partir da maior carta
+            if (jogadores[i].GetMao()[1] > jogadores[indice].GetMao()[1]) {
+                indice = i;
+                // impossivel desempatar
+            } else {
+                nr_vencedores++;
+            }
+        }
+    }
+
+    return nr_vencedores;
+}
+
 int main() {
     int numero_de_rodadas, dinheiro_inicial;
-    int numero_de_jogadores, valor_do_pingo, valor_aposta;
+    int numero_de_jogadores, valor_do_pingo;
+    int valor_aposta, pote;
     string nome_jogador, jogada, carta;
-    Jogador *jogadores;
+    string valor_mao;
+    string nome_vencedor, jogada_vencedor;
+    bool invalida = false;
+    Jogador *jogadores, vencedor, *vencedores;
 
-    ifstream arquivo("entrada2.txt");
+    ifstream arquivo("entrada.txt");
 
     arquivo >> numero_de_rodadas >> dinheiro_inicial;
 
     for (int i = 0; i < numero_de_rodadas; i++) {
+        pote = 0;
+        int numero_de_vencedores = 0;
+
         arquivo >> numero_de_jogadores >> valor_do_pingo;
+
+        jogadores = new Jogador[numero_de_jogadores + 1];
+        vencedores = new Jogador[numero_de_jogadores + 1];
 
         for (int j = 0; j < numero_de_jogadores; j++) {
             arquivo >> nome_jogador >> valor_aposta;
 
-            jogadores = new Jogador[numero_de_jogadores + 1];
+            jogadores[j].SetNome(nome_jogador);
+            jogadores[j].SetDinheiro(dinheiro_inicial);
+            invalida = jogadores[j].SetAposta(valor_aposta + valor_do_pingo);  // se o jogador apostar o que nao tem, a rodada eh invalidade
+
+            pote += valor_aposta + valor_do_pingo;
 
             for (int k = 0; k < 5; k++) {
                 arquivo >> carta;
@@ -29,10 +120,33 @@ int main() {
                 jogadores[j].AdicionarCarta(carta);
             }
 
-            cout << "Jogador " << j << ": ";
-            cout << jogadores[j].GetMao() << endl;
+            valor_mao = jogadores[j].GetMao()[0];
+            // cout << valor_mao << endl;
+            jogadores[j].SetValorMao(valor_mao);
+        }
 
-            jogadores[j].ImprimirMao();
+        numero_de_vencedores = GetNrVencedores(jogadores, numero_de_jogadores);
+
+        if (numero_de_vencedores == 1) {
+            vencedor = GetVencedor(jogadores, numero_de_jogadores);
+
+            nome_vencedor = vencedor.GetNome();
+            jogada_vencedor = vencedor.GetMao()[0];
+            cout << numero_de_vencedores << " " << jogada_vencedor << endl;
+            cout << nome_vencedor << endl;
+        } else {
+            vencedores = GetVencedores(jogadores, numero_de_vencedores);
+
+            cout << numero_de_vencedores << " " << jogada_vencedor << " " << endl;
+
+            for (int i = 0; i < numero_de_vencedores; i++) {
+                cout << vencedores[i].GetNome() << " ";
+            }
+        }
+
+        // toda rodada a mao do jogador sera diferente
+        for (int l = 0; l < numero_de_jogadores; l++) {
+            jogadores[l].LimparMao();
         }
     }
 
