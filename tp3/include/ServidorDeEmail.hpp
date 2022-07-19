a107c472af2b06feb0db366afa4aceea2f29c8c6
Vinícius Cardoso <vinicius.mct17@gmail.com>
2022-07-19 00:55:57 -0300

    finalizando o projeto

--- tp3/include/ServidorDeEmail.hpp
+++ tp3/include/ServidorDeEmail.hpp
@@ -15,82 +15,16 @@ class ServidorDeEmail {
    private:
     int tamanho_tabela;
     int Hash(int chave);
-    // ArvoreBinaria Tabela[5];
 
    public:
     ServidorDeEmail(int tamanho_tabela);
 
-    // void Iniciar(string nome_arquivo_entrada, string nome_arquivo_saida);
     void Iniciar(ifstream &arquivo_entrada, ofstream &arquivo_saida);
-    void Entrega(int id_destinatario, int id_email, string mensagem, ArvoreBinaria *Tabela);
     void Inserir(Email *email, ArvoreBinaria *Tabela, int id_destinatario);
     int Pesquisar(int chave, ArvoreBinaria *Tabela);
-    void Imprime(ArvoreBinaria *Tabela, int tamanho);
+    void Entrega(int id_destinatario, int id_email, string mensagem, ArvoreBinaria *Tabela);
+    string Consulta(int id_destinatario, int id_email, ArvoreBinaria *Tabela);
+    bool Apaga(int id_destinatario, int id_email, ArvoreBinaria *Tabela);
 };
-
-// class Hash_LE {
-//    private:
-//     int Hash(int chave);
-//     ListaEncadeada Tabela[TAM];
-
-//    public:
-//     Hash_LE();
-//     int Pesquisa(int chave);
-//     void Insere(int item);
-//     void Remove(int chave);
-//     void Imprime();
-// }
-// ;
-
-// Hash_LE::Hash_LE() {
-// }
-
-// int Hash_LE::Hash(int chave) {
-//     return chave % TAM;
-// }
-
-// int Hash_LE::Pesquisa(int chave) {
-//     int item;
-//     int pos;
-
-//     pos = Hash(chave);
-//     item = Tabela[pos].Pesquisa(chave);
-
-//     return item;
-// }
-
-// void Hash_LE::Remove(int chave) {
-//     int pos;
-
-//     pos = Hash(chave);
-
-//     Tabela[pos].RemovePosicao(chave);
-// }
-
-// void Hash_LE::Imprime() {
-//     for (int i = 0; i < TAM; i++) {
-//         Tabela[i].Imprime();
-//     }
-// }
-
-// int main() {
-//     Hash_LE hash;
-
-//     for (int i = 0; i < 100; i++) {
-//         hash.Insere(i);
-//     }
-
-//     hash.Imprime();
-
-//     cout << "-----" << endl;
-
-//     for (int i = 0; i < 14; i++) {
-//         hash.Remove(i);
-//     }
-
-//     hash.Imprime();
-
-//     return 0;
-// }
 
 #endif
--- tp3/src/ServidorDeEmail.cpp
+++ tp3/src/ServidorDeEmail.cpp
@@ -24,23 +24,33 @@ void ServidorDeEmail::Inserir(Email *email, ArvoreBinaria *Tabela, int id_destin
 
     aux = Pesquisar(id_destinatario, Tabela);
 
-    if (aux != -1)
-        throw("Erro: item ja esta presente");
+    // retorna um erro se o usuario tentar inserir um item que ja esta presente
+    erroAssert(aux == -1, "Erro: Este item ja esta presente");
 
     pos = Hash(id_destinatario);
 
     Tabela[pos].Inserir(email);
 }
 
-void ServidorDeEmail::Imprime(ArvoreBinaria *Tabela, int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        if (Tabela[i].IsVazia())
-            cout << i << ": vazia" << endl;
-        else {
-            cout << i << ": ";
-            Tabela[i].Caminha(2);
-        }
-    }
+void ServidorDeEmail::Entrega(int id_destinatario, int id_email, string mensagem, ArvoreBinaria *Tabela) {
+    Email *email = new Email(id_email, id_destinatario, mensagem);
+
+    Inserir(email, Tabela, id_destinatario);
+}
+
+string ServidorDeEmail::Consulta(int id_destinatario, int id_email, ArvoreBinaria *Tabela) {
+    string mensagem = Tabela[Hash(id_destinatario)].PesquisarEmail(id_email, id_destinatario);
+
+    return mensagem;
+}
+
+bool ServidorDeEmail::Apaga(int id_destinatario, int id_email, ArvoreBinaria *Tabela) {
+    bool mensagem_existe = false;
+    int hash = Hash(id_destinatario);
+
+    mensagem_existe = Tabela[hash].Apagar(id_email);
+
+    return mensagem_existe;
 }
 
 void ServidorDeEmail::Iniciar(ifstream &arquivo_entrada, ofstream &arquivo_saida) {
@@ -58,30 +68,41 @@ void ServidorDeEmail::Iniciar(ifstream &arquivo_entrada, ofstream &arquivo_saida
 
         arquivo_entrada >> id_destinatario;
         arquivo_entrada >> id_email;
-        arquivo_entrada >> nr_palavras;
 
         string mensagem;
 
-        // adiciona cada linha de palavras a uma unica frase
-        for (int i = 0; i < nr_palavras; i++) {
-            arquivo_entrada >> linha;
-            mensagem += linha;
+        // so le a mensagem quando a operacao eh de entrega
+        if (operacao == "ENTREGA") {
+            arquivo_entrada >> nr_palavras;
 
-            // adiciona espacos entre as palavras, menos ao final da ultima palavra
-            if (i != nr_palavras - 1)
-                mensagem += " ";
+            // adiciona cada linha de palavras a uma unica frase
+            for (int i = 0; i < nr_palavras; i++) {
+                arquivo_entrada >> linha;
+                mensagem += linha;
+
+                // adiciona espacos entre as palavras, menos ao final da ultima palavra
+                if (i != nr_palavras - 1)
+                    mensagem += " ";
+            }
         }
 
         if (operacao == "ENTREGA") {
             Entrega(id_destinatario, id_email, mensagem, Tabela);
-        }
+
+            arquivo_saida << "OK: MENSAGEM " << id_email << " PARA " << id_destinatario << " ARMAZENADA EM " << Hash(id_destinatario) << endl;
+        } else if (operacao == "CONSULTA") {
+            string mensagem = Consulta(id_destinatario, id_email, Tabela);
+
+            if (mensagem != "")
+                arquivo_saida << "CONSULTA " << id_destinatario << " " << id_email << ": " << mensagem << endl;
+            else
+                arquivo_saida << "CONSULTA " << id_destinatario << " " << id_email << ": MENSAGEM INEXISTENTE" << endl;
+        } else if (operacao == "APAGA") {
+            if (Apaga(id_destinatario, id_email, Tabela))
+                arquivo_saida << "OK: MENSAGEM APAGADA" << endl;
+            else
+                arquivo_saida << "ERRO: MENSAGEM INEXISTENTE" << endl;
+        } else
+            throw "Erro: Operacao invalida no arquivo";
     }
-
-    Imprime(Tabela, tamanho_tabela);
-}
-
-void ServidorDeEmail::Entrega(int id_destinatario, int id_email, string mensagem, ArvoreBinaria *Tabela) {
-    Email *email = new Email(id_email, mensagem);
-
-    Inserir(email, Tabela, id_destinatario);
 }
